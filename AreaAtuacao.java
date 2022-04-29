99214e19a5ff7db842aa21ad28af799b9744bfa2
Rafael Mafort <101189058+raafamafort@users.noreply.github.com>
2022-04-29 11:47:27 -0300

    add classes

---
+++ AreaAtuacao.java
@@ -0,0 +1,24 @@
+package codify;
+
+public class AreaAtuacao extends Tecnologia {
+
+	private String codigoArea;
+	private String nome;
+
+	public String getCodigoArea() {
+		return codigoArea;
+	}
+
+	public void setCodigoArea(String codigoArea) {
+		this.codigoArea = codigoArea;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
