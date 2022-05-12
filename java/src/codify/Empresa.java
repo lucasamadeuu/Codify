72c0aac201f0d6cf31eee16b42aa9ad23c144890
lucasamadeuu <lucas.amadeu.soares@gmail.com>
2022-05-11 21:52:49 -0300

    fixing empresa

--- java/src/codify/Empresa.java
+++ java/src/codify/Empresa.java
@@ -4,24 +4,33 @@ import javax.swing.JOptionPane;
 
 public class Empresa extends Usuario{
 		
-		private String empresaId;
-
-		public String getEmpresaId() {
-			return empresaId;
-		}
-
-		public void setEmpresaId(String empresaId) {
-			this.empresaId = empresaId;
-		}
+		private int CNPJ;
+		private String email;
 		
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public int getCNPJ() {
+			return CNPJ;
+		}
+
+		public void setCNPJ(int cNPJ) {
+			CNPJ = cNPJ;
+		}
+
 		void loginEmpresa() {
 			
 			Empresa logar = new Empresa();
 			
-			logar.setEmpresaId(JOptionPane.showInputDialog("Login - Empresa \nDigite seu ID"));
+			logar.setEmail(JOptionPane.showInputDialog("Login - Empresa \nDigite seu email"));
 			logar.setSenha(JOptionPane.showInputDialog("Login - Empresa \nDigite sua senha"));
 			
-			System.out.println("ID Empresa: " + logar.getEmpresaId() + "\nSenha: " + logar.getSenha());
+			System.out.println("E-mail Empresa: " + logar.getEmail() + "\nSenha: " + logar.getSenha());
 			System.out.println(
 					"------------------------------------------\nLogin Empresa concluído!\n------------------------------------------");
 			
